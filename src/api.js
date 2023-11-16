6914258db3bc331925e2dc52d178c386e85f37fd
sbharathram23 <sbharathram23@gmail.com>
2023-11-16 11:28:22 +0530

    added url

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const headerWithToken = axios.create({
     // baseURL: 'https://devapi.tell.navadhiti.com',
-    baseURL: 'https://b0d1-2402-3a80-4a-3ed2-964c-dc8b-be46-ea7.ngrok-free.app',
+    baseURL: 'https://5583-2402-3a80-4a-3ed2-964c-dc8b-be46-ea7.ngrok-free.app',
 });
 
 headerWithToken.interceptors.request.use((config) => {
