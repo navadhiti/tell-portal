c31d23194e7942894941140a73e985313f3c53b7
palani <palani@navadhiti.com>
2023-11-16 17:21:27 +0530

    changed the baseapi

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const headerWithToken = axios.create({
-    // baseURL: 'https://devapi.tell.navadhiti.com',
-    baseURL: 'https://e07d-2402-3a80-30-33d2-37f-284f-6387-43b8.ngrok-free.app',
+    baseURL: 'https://devapi.tell.navadhiti.com'
+    // baseURL: 'https://e07d-2402-3a80-30-33d2-37f-284f-6387-43b8.ngrok-free.app',
 });
 
 
